ef0e1437baad622a4d4c94f486be66911c981a34
Kydyrali_neproggerr <mr.kydyrali02@gmail.com>
2024-04-16 09:44:49 +0500

    DockerFile Uploaded V3.0

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,2 +1,2 @@
-export const Base_URL = 'http://localhost:8080/api/v1';
-//export const Base_URL = 'https://teacher-forge-production.up.railway.app/api/v1';
+//export const Base_URL = 'http://localhost:8080/api/v1';
+export const Base_URL = 'https://teacher-forge-production.up.railway.app/api/v1';
